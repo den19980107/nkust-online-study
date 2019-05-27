a96148f36f794d684d37db7bca8f03d611aa7baf
Zeng-Bo-Han <den19980017>
2019-05-27 23:08:54 +0800

    新增一進入觀看影片頁面會直接顯示rfm

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://den19980107:kkc060500@ds161104.mlab.com:61104/online-study',
+    database: 'mongodb://i-Coding:password123@ds161104.mlab.com:61104/online-study',
     secret: 'yoursecret'
 }
